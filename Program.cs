2ec1ea060615bddfd00e1db59a954aef9b05b5a9
GMrlsN <111917972+GMrlsN@users.noreply.github.com>
2022-11-14 09:45:24 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Gabriel Morales Nuñez
+using System;
+using System.IO;
+
+namespace Semantica
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                using Lenguaje a = new Lenguaje();
+                a.Programa();
+                //a.Dispose();
+                //GC.Collect();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
